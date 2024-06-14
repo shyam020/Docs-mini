e1930968534ba994f3d2a95b589b37c6abd469e7
shyam020 <patelshyam18352@gmail.com>
2024-06-14 21:12:17 +0530

    first commit

---
+++ Docs-mini-app/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <>
+      <div className='w-full bg-zinc-800 flex justify-center pt-10 text-zinc-400'>Document</div>
+      <div className='w-full h-screen bg-zinc-800 flex justify-center items-center text-zinc-400 '>
+      <h1 className='text-[15vw]'>Docs</h1>
+      </div>
+  
+   </>
+  )
+}
+
+export default App
+
+
